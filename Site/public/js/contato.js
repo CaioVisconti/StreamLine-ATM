20e99e45ac844b828725baf3694874fe82e3fd67
CaioVisconti <caio.visconti@sptech.school>
2025-04-03 21:48:54 -0300

    Funcionamento da API Email.js da tela de contato

--- Site/public/js/contato.js
+++ Site/public/js/contato.js
@@ -1,16 +1,15 @@
 (function() {
-    emailjs.init('RJKvIXvcHc6QANjdS'); // inicializa o emailjs
+    emailjs.init('Tujljmq8h8RLf4Va5'); // inicializa o emailjs
   })();
 
   document.querySelector('.botaoEnviar').addEventListener('click', function() { // escuta o clique do botao enviar
     const nome = document.getElementById('inputName').value;
     const email = document.getElementById('inputEmail').value;
     const mensagem = document.getElementById('inputDuvidas').value;
-    const divmsg = document.getElementById('divmsg');
   
     // verifica se os campos estao vazios
     if (!nome || !email || !mensagem) {
-      divmsg.innerHTML = "<br> <p style='color:white;'>Preencha todos os campos!</p>";
+      alert("Preencha todos os campos!");
       return;
     }
   
@@ -20,16 +19,16 @@
       email: email,
       mensagem: mensagem
     };
-  
-    emailjs.send('service_7j9wchg', 'template_0aefsto', params) // requisicao p enviar
+
+    emailjs.send('service_g92keif', 'template_6lylonf', params) // requisicao p enviar
       .then(function(response) {
         console.log('SUCCESS!', response.status, response.text); // requisicao bem sucedida
-        divmsg.innerHTML = "<br> <p style='color:white;'>Mensagem enviada com sucesso!</p>";
+        alert("Mensagem enviada com sucesso!");
         document.getElementById('inputName').value = ''; // limpa os campos
         document.getElementById('inputEmail').value = '';
         document.getElementById('inputDuvidas').value = '';
       }, function(error) { // caso seja devolvido um erro
         console.error('FAILED...', error);
-        divmsg.innerHTML = "<br> <p style='color:white;'>Erro ao enviar a mensagem. Tente novamente.</p>";
+        alert("Erro ao enviar a mensagem. Tente novamente.");
       });
 });
