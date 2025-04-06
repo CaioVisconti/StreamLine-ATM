30fd995752dec556e6cffd1c9726491e2002480e
GuinhoFSilva <guilherme.silva@sptech.school>
2025-04-06 09:35:44 -0300

    Criando modal de edição

--- Site/public/js/cadastroAgencia.js
+++ Site/public/js/cadastroAgencia.js
@@ -1,5 +1,21 @@
-function mostrarModal() {
+function mostrarModalCad() {
     const modal = document.querySelector(".modal");
+    const fade = document.querySelector(".fade");
+    if (modal.style.display == "none") {
+        modal.style.display = "flex";
+        fade.style.display = "block";
+    } else {
+        const alert = confirm("Gostaria de fechar o modal?");
+        if (alert) {
+            modal.style.display = "none"
+            fade.style.display = "none";
+
+        }
+    }
+}
+
+function mostrarModalEdit() {
+    const modal = document.querySelector(".modal-edit");
     const fade = document.querySelector(".fade");
     if (modal.style.display == "none") {
         modal.style.display = "flex";
