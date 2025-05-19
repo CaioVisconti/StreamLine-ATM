3d2325ef414a306ed7d5efbb4e9a6b1e5442bbc7
ni-teixeira <nicoly.teixeira04@gmail.com>
2025-05-18 21:37:43 -0300

    script jira arrumado

--- Middleware-Jira/script-jira.py
+++ Middleware-Jira/script-jira.py
@@ -1,15 +1,15 @@
 from jira import JIRA
 
-jira_options = {'server': 'https://streamlineatm-1741820046277.atlassian.net/jira/your-work'}
-jira_login = {'username': 'streamlineatm@gmail.com', 'password': 'SPTechUrubu100'}
+jira_options = {'server': 'https://bancostreamline.atlassian.net/'}
+jira_login = {'username': 'bancostreamline@gmail.com', 'password': 'Urubu@100'}
 jira = JIRA(options=jira_options, **jira_login)
 
 issue_fields = {
-    'project': {'key': 'PROJECT_KEY'},
-    'issuetype': {'name': 'Bug'},
-    'summary': 'Bug na tela de login',
-    'description': 'Detalhes do bug',
-    'priority': {'name': 'High'}
+    'project': {'key': 'G1ALERTAS'},
+    'issuetype': {'name': 'Alertas de hardware'},
+    'summary': f'Falha de {componente} no atm {fkAtm}',
+    'description': f'O ATM de ID {fkAtm} apresentou falha no {componente} no valor de {valor}.',
+    'priority': {'name': categoria}
 }
 
 new_issue = jira.create_issue(fields=issue_fields)
